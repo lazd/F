2d1ee5148beab5e61a0b3958d4763484d08705bb
Larry Davis <lazd@lazd.net>
2012-07-25 01:33:18 -0700

    New docs and rolled up F.js

--- F/js/extensions/String.js
+++
@@ -1,7 +0,0 @@
-String.prototype.decapitalize = function() {
-	return this.slice(0, 1).toLowerCase()+this.slice(1);
-};
-
-String.prototype.capitalize = function() {
-	return this.slice(0, 1).toUpperCase()+this.slice(1);
-};
