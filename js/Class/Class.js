1b1bdc7a8f518f9b7b942d1c56cd99f52afb2e09
Larry Davis <lawdavis@adobe.com>
2012-07-26 12:13:24 -0700

    Added @author for Object.create polyfill

--- js/Class/Class.js
+++ js/Class/Class.js
@@ -196,8 +196,11 @@ function Class(descriptor) {
 
 if (!Object.create) {
 	/**
-	 * Creates a new object with the specified prototype object
-	 * @param {Object} o the prototype to use
+	 * Polyfill for Object.create. Creates a new object with the specified prototype object.
+	 * 
+	 * @author Mozilla MDN https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/create/
+	 *
+	 * @param {Object} o	The prototype to create a new object with
 	 */
 	Object.create = function (o) {
 		if (arguments.length > 1) {
