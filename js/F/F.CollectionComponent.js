a926494a6478bd188f17add0c78e35d6e67029d6
Larry Davis <lawdavis@adobe.com>
2012-08-21 13:11:18 -0700

    Added collection: prefix for events, added fetch(), changed load() to not fetch data from server, added removal of collection listeners on destruct, removed getCollection() in favor of this.collection, added event documentation

--- js/F/F.CollectionComponent.js
+++ js/F/F.CollectionComponent.js
@@ -21,19 +21,12 @@ F.CollectionComponent = new Class(/** @lends F.CollectionComponent# */{
 			'Collection'
 		]);
 		
-		// Create a collection
-		this.collection = new this.Collection();
-		
 		// Bind for use as listeners
 		this.bind(this.addModel);
 		this.bind(this.removeModel);
+		this.bind(this.render);
 		
-		// Re-render when the collection is fetched, items are added or removed
-		this.collection.on('add', this.addModel);
-		this.collection.on('remove', this.removeModel);
-		this.collection.on('loaded', this.render); // custom event we call after fetches
-		// Don't re-render on change! let the sub-views do that
-		// this.collection.on('change', this.render);
+		this._useCollection(new this.Collection());
 		
 		// Default parameters are the prototype params + options params
 		this.defaultParams = _.extend({}, this.defaultParams, options.defaultParams);
@@ -45,15 +38,31 @@ F.CollectionComponent = new Class(/** @lends F.CollectionComponent# */{
 		this.collectionLoaded = false;
 	},
 	
+	destruct: function() {
+		this._releaseCollection();
+	},
+	
 	Collection: Backbone.Collection,
 	
-	/**
-	 * Get the collection associated with this component
-	 *
-	 * @returns {Backbone.Collection}	The collection associated with this component
-	 */
-	getCollection: function() {
-		return this.collection;
+	_useCollection: function(collection) {
+		// Store collection
+		this.collection = collection;
+		
+		// Re-render when the collection is fetched, items are added or removed
+		this.collection.on('add', this.addModel);
+		this.collection.on('remove', this.removeModel);
+		this.collection.on('loaded', this.render); // custom event we call after fetches
+		// this.collection.on('change', this.render); // Don't re-render on change! let the sub-views do that
+	},
+	
+	_releaseCollection: function() {
+		// Unbind events
+		this.collection.off('add', this.addModel);
+		this.collection.off('remove', this.removeModel);
+		this.collection.off('loaded', this.render); // custom event we call after fetches
+		
+		// Remove reference to collection
+		this.collection = null;
 	},
 	
 	/**
@@ -64,8 +73,8 @@ F.CollectionComponent = new Class(/** @lends F.CollectionComponent# */{
 	 * @returns {F.CollectionComponent}	this, chainable
 	 */
 	refresh: function(callback) {
-		// Just load the collection with the current params
-		this.load(this.params, callback);
+		// Just fetch the collection with the current params
+		this.fetch(this.params, callback);
 		
 		return this;
 	},
@@ -92,14 +101,14 @@ F.CollectionComponent = new Class(/** @lends F.CollectionComponent# */{
 	},
 	
 	/**
-	 * Fetch the collection with optional 
+	 * Fetch the collection by fetching it from the server
 	 *
-	 * @param {Object} fetchParams	Optional parameters to pass when fetching
-	 * @param {Function} callback	Optional callback to execute on successful fetch
+	 * @param {Object} [fetchParams]	Parameters to pass when fetching
+	 * @param {Function} [callback]		Callback to execute on successful fetch
 	 *
 	 * @returns {F.CollectionComponent}	this, chainable
 	 */
-	load: function(fetchParams, callback) {
+	fetch: function(fetchParams, callback) {
 		// Combine new params, if any, with defaults and store, overwriting previous params
 		if (fetchParams)
 			this.params = _.extend({}, this.defaultParams, fetchParams);
@@ -114,7 +123,7 @@ F.CollectionComponent = new Class(/** @lends F.CollectionComponent# */{
 				this.collection.trigger('loaded');
 
 				// Component event
-				this.trigger('collectionLoaded');
+				this.trigger('collection:loaded', this.collection);
 				this.collectionLoaded = true;
 				
 				if (typeof callback === 'function')
@@ -124,6 +133,26 @@ F.CollectionComponent = new Class(/** @lends F.CollectionComponent# */{
 		
 		return this;
 	},
+	
+	
+	/**
+	 * Load a Backbone.Collection directly or create a collection from an array of data
+	 *
+	 * @param {mixed} collectionOrData	Backbone.Collection to load or Array of Objects with data to create the collection from
+	 *
+	 * @returns {F.CollectionComponent}	this, chainable
+	 */
+	load: function(collectionOrData) {
+		this._releaseCollection();
+
+		if (collectionOrData instanceof Backbone.Collection)
+			this._useCollection(collectionOrData);
+		else
+			this._useCollection(new this.Collection(collectionOrData));
+		
+		return this;
+	},
+	
 	
 	/**
 	 * Show this component. Provide options.params to fetch with new parameters. The collection will be fetched before showing if it hasn't already
@@ -135,8 +164,8 @@ F.CollectionComponent = new Class(/** @lends F.CollectionComponent# */{
 	show: function(options) {
 		options = options || {};
 		if (options.params) {
-			// Load the collection by itemId
-			this.load(options.params, function() {
+			// Fetch the collection from the server
+			this.fetch(options.params, function() {
 				this.show({
 					silent: options.silent
 				});
@@ -152,7 +181,17 @@ F.CollectionComponent = new Class(/** @lends F.CollectionComponent# */{
 		}
 		else
 			this.inherited(arguments);
-			
+		
 		return this;
 	}
+	
+	
+	/**
+	 * Triggered when the collection is loaded from the server
+	 *
+	 * @name F.CollectionComponent#collection:loaded
+	 * @event
+	 *
+	 * @param {Backbone.Collection}	collection	The collection that was loaded
+	 */
 });
